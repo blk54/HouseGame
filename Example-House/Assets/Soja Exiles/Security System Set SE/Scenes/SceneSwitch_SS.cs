2ec1a161393b52e584124e32119ea4133dbb00dd
blk54 <47371275+blk54@users.noreply.github.com>
2020-11-21 15:32:44 -0500

    assets

---
+++ Example-House/Assets/Soja Exiles/Security System Set SE/Scenes/SceneSwitch_SS.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch_SS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+			SceneManager.LoadScene("Demo 01");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+			SceneManager.LoadScene("Demo 02");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+			SceneManager.LoadScene("Demo 03");
+        }
+
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			SceneManager.LoadScene("Cameras Screens and Textures Demo");
+		}
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
